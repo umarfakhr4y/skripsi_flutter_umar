b0089ae0b022714c2166f7b4f0b65a88356ce201
Umar <umarfakhr4y@gmail.com>
2026-02-24 06:00:44 +0700

    [ADD] media query

---
+++ lib/conn/auth.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+import '../size_helper.dart';
+
+part '../main.dart';
+part '../splashscreen.dart';
+part '../page/loginPage.dart';
