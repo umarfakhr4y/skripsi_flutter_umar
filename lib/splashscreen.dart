7c183cf3adfb3dd6a08bd415c24b727b71ffd456
Umar <umarfakhr4y@gmail.com>
2026-02-24 06:01:01 +0700

    [ADD] bikin polosan splash sama login

---
+++ lib/splashscreen.dart
@@ -0,0 +1,38 @@
+part of 'conn/auth.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 1),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => loginPage()),
+      ),
+    );
+    ;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Container(
+          height: displayHeight(context) * 0.3,
+          width: displayHeight(context) * 0.3,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.lightBlue[100],
+          ),
+        ),
+      ),
+    );
+  }
+}
